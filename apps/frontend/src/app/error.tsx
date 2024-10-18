c30d430032dc6928eb8277617df894aa1a355f3a
nobel912 <nobel@grype.ca>
2024-10-19 01:33:25 +0600

    frontend project structure implemented

---
+++ apps/frontend/src/app/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const ErrorPage = () => {
+  return (
+    <div className="h-[100vh] flex justify-center items-center text-red-500">
+      <h1>Something went wrong!</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
